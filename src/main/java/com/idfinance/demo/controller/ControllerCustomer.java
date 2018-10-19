3e136a6375d96bb68a70d25b99753290f229afb7
AlexandrSi <snaiper.bla@gmail.com>
2018-10-19 12:24:20 +0300

    Send customer request to DB

--- src/main/java/com/idfinance/demo/controller/ControllerCustomer.java
+++ src/main/java/com/idfinance/demo/controller/ControllerCustomer.java
@@ -1,16 +1,38 @@
 package com.idfinance.demo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.idfinance.demo.model.*;
 import com.idfinance.demo.repository.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @Controller
 public class ControllerCustomer {
+
+    @Autowired
+    private CustomerRepo customerRepo;
 
     @GetMapping ("/customer")
     public String customer(){
         return "customer";
     }
+
+    @RequestMapping (value = "/sendStmt", method = RequestMethod.GET)
+    public String custStmt(@RequestParam(value = "json") String customStmt) throws IOException {
+          ObjectMapper mapper = new ObjectMapper();
+          CustomerStatement customerStatement = mapper.readValue(customStmt, CustomerStatement.class);
+          customerRepo.save(customerStatement);
+        return "customer";
+    }
+
+//    @RequestMapping (value = "/sendStmt", method = RequestMethod.GET)
+//    public String custStmt(@RequestParam(value = "json") String customStmt) throws IOException {
+//        ObjectMapper mapper = new ObjectMapper();
+//        CustomerStatement customerStatement = mapper.readValue(customStmt, CustomerStatement.class);
+//        customerRepo.save(customerStatement);
+//        return "customer";
+//    }
 }
--- src/main/webapp/WEB-INF/pages/customer.jsp
+++ src/main/webapp/WEB-INF/pages/customer.jsp
@@ -1,10 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
 <html>
-<head></head>
+<head>
+
+</head>
 
 <body>
-<form action="sendStmt" method="post" enctype="application/json">
-    <textarea></textarea>
-    <button>Отправить</button>
+<form action="/sendStmt" method="get">
+    <p><textarea name="json" cols="48" rows="8"> </textarea></p>
+    <p><input type="submit"></p>
+    <p><input type="submit" value="Статус"></p>
 </form>
 </body>
 </html>
